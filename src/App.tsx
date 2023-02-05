8c31fc53041dde7733a057dde78d03b2fb544c3c
Asel <asel.alimbaeva.kk@gmail.com>
2023-02-05 08:09:21 +0300

    feat: add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Banner } from './components/banner/Banner';
+import { Footer } from './components/footer/Footer';
 import { MainPageCatalog } from './components/mainPageCatalog/MainPageCatalog';
 import { SellSlide } from './components/sellSlide/SellSlide';
 import './index.scss';
@@ -10,6 +11,7 @@ function App() {
       <Banner />
       <SellSlide />
       <MainPageCatalog />
+      <Footer />
     </div>
   );
 }
