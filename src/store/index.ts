1bb19c04b3061c4768748134bd0b8ed950c79d6a
Asel <asel.alimbaeva.kk@gmail.com>
2023-02-08 13:48:47 +0300

    feat: add langeage reducer

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,1 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
+import languageReducer from './languageReducer';
+
+export const store = configureStore({
+  reducer: {
+    languageReducer: languageReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
