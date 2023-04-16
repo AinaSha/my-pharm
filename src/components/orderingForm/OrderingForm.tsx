5c8c96f411fcab8ebbdb79c62aa17758db8caf1d
Ainagul Shabdanova <shabdanova.ainagul.kk@gmail.com>
2023-04-16 13:29:27 +0600

    feat: update order page and add adpative

--- src/components/orderingForm/OrderingForm.tsx
+++ src/components/orderingForm/OrderingForm.tsx
@@ -15,11 +15,11 @@ export const OrderingForm: FC = () => {
       <h3>Оплата и доставка</h3>
       <form className="order-form">
         <div className="user-name">
-          <div className="form-item">
+          <div className="form-item user-name__form-item">
             <label htmlFor="">Имя:</label>
             <input className="user-name__input" type="text" />
           </div>
-          <div className="form-item">
+          <div className="form-item user-name__form-item last-name-form">
             <label htmlFor="">Фамилия:</label>
             <input className="user-name__input" type="text" />
           </div>
