06649c8eeda081f0c82a62015d1747401b132713
Ainagul Shabdanova <shabdanova.ainagul.kk@gmail.com>
2023-05-24 12:12:56 +0600

    feat: update

--- src/pages/promotion/promotion.tsx
+++ src/pages/promotion/promotion.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store';
@@ -11,28 +11,60 @@ import './promotion.scss';
 
 export const Promotion: FC = () => {
   const { products } = useSelector((state: RootState) => state.ProductsReducer);
-  console.log(products);
-  const renderCardItems = () => {
-    return products.map((el: IProduct) => {
+  const cardsOnPage = 2;
+  const [allCards, setAllCards] = useState<IProduct[]>([]);
+  const [curentPage, setCurentPage] = useState('1');
+  const allPageNumbers = Math.ceil(allCards.length / cardsOnPage);
+  const [cards, setCards] = useState<IProduct[]>(allCards.slice(0, cardsOnPage));
+
+  const renderCard = (el: IProduct) => {
+    return (
+      <Link to={`/products/${el.name}__${el.id}`} key={el.id} className="promotion-gallery__card">
+        <div className="promotion-gallery__card-inner">
+          <img src={el.image} alt="discount ites" />
+          <p className="promotion-date">до 31 мая</p>
+          <p className="promotion-price">
+            {el.discount_price} сом<span>{el.price} сом</span>
+          </p>
+          <p className="promotion-desc">{el.name}</p>
+        </div>
+      </Link>
+    );
+  };
+
+  useEffect(() => {
+    const allCard: IProduct[] = [];
+    products.map((el: IProduct) => {
       if (el.in_stock && el.discount_price) {
-        return (
-          <Link
-            to={`/products/${el.name}__${el.id}`}
-            key={el.id}
-            className="promotion-gallery__card"
-          >
-            <div className="promotion-gallery__card-inner">
-              <img src={el.image} alt="discount ites" />
-              <p className="promotion-date">до 31 мая</p>
-              <p className="promotion-price">
-                {el.discount_price} сом<span>{el.price} сом</span>
-              </p>
-              <p className="promotion-desc">{el.name}</p>
-            </div>
-          </Link>
-        );
+        allCards.push(el);
       }
     });
+    setAllCards(allCard);
+    setCards(allCard.slice(0, cardsOnPage));
+  }, [products]);
+
+  const paginate = (
+    pageNumber: number,
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent> | null,
+    btnChilde: ChildNode | null | undefined
+  ) => {
+    let btn: HTMLButtonElement;
+    if (!!e) btn = e.currentTarget as HTMLButtonElement;
+    const buttons: ChildNode | NodeListOf<ChildNode> | undefined = btnChilde
+      ? btnChilde.childNodes
+      : btn!.parentElement?.childNodes;
+    if (!buttons) return;
+    buttons.forEach((el: ChildNode) => {
+      const btn = el as HTMLButtonElement;
+      btn.classList.remove('active-btn');
+
+      if (btn.textContent === String(pageNumber)) btn.classList.add('active-btn');
+    });
+
+    const start = (pageNumber - 1) * cardsOnPage;
+    const end = start + cardsOnPage;
+    setCurentPage(String(pageNumber));
+    setCards(allCards.slice(start, end));
   };
 
   return (
@@ -41,9 +73,15 @@ export const Promotion: FC = () => {
         <Breadcrumbs homeLabel="Home" />
         <h3>Акции и предложения</h3>
         <div className="promotion-gallery">
-          <div className="promotion-gallery__inner">{renderCardItems()}</div>
+          <div className="promotion-gallery__inner">
+            {cards.map((el) => {
+              return renderCard(el);
+            })}
+          </div>
         </div>
-        {/* <Pagination /> */}
+        {allPageNumbers > 1 && (
+          <Pagination allPageNumbers={allPageNumbers} paginate={paginate} curentPage={curentPage} />
+        )}
         <Pharmacies />
       </div>
     </div>
