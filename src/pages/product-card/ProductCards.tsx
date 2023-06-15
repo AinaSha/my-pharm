333dd5443a320040bd8ebbe7df29438c24d4486e
Asel <asel.alimbaeva.kk@gmail.com>
2023-06-15 15:03:20 +0300

    fix: section style

--- src/pages/product-card/ProductCards.tsx
+++ src/pages/product-card/ProductCards.tsx
@@ -218,7 +218,7 @@ export const ProductCards: FC = () => {
           <p>{translate.pharmacyName}</p>
         </div>
       </div>
-      <section>
+      <section className="map">
         <MapYandex />
         <ListCoordinat />
       </section>
